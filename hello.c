a06ccf168f379963e22b9e997c12c2f4e9fdd4e7
ViniciusFM <viniciusmaciel@iftm.edu.br>
2019-03-13 13:43:52 -0300

    somando numeros

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
   printf("hello, %s\n", argv[1]);
+  if(argc == 4)
+  {
+    int a = atoi(argv[2]);
+    int b = atoi(argv[3]);
+    printf("soma:%d\n", a+b);
+  }
   return 0;
 }
